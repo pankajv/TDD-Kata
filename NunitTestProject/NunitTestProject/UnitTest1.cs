686e3b9553bb3d17d9bd4bc7314b4cf4952572dc
Pankaj Verma <pankaj.spn2noida@gmail.com>
2011-11-09 14:06:17 +0530

    First NUnit Test Project On GitHub

---
+++ NunitTestProject/NunitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NunitTestProject
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        [TestCase("")]
+        public void Add_EmptyString(string strValue)
+        {
+        }
+    }
+}
